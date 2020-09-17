46bfecb9dcd3251aafb5ec75905cb1057741dd6a
estuart3 <70380230+estuart3@users.noreply.github.com>
2020-09-17 19:03:53 -0400

    ran shapiro-wilkes test'

--- activity 3.R
+++ activity 3.R
@@ -3,3 +3,5 @@ ch4 <- read.csv("/Users/emmastuart/Documents/a03/lemming_herbivory.csv")
 ch4$herbivory <- as.factor(ch4$herbivory)
 plot(ch4$CH4_Flux ~ ch4$herbivory, xlab = "Treatment", 
      ylab = "CH4 fluxes (mgC m -2 day-1)")
+#Shapiro-Wilkes Test
+shapiro.test(ch4$CH4_Flux[ch4$herbivory == "Ctl"])
