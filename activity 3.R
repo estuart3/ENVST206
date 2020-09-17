55e9be3df506c248e539b4ed75719812624f77bd
estuart3 <70380230+estuart3@users.noreply.github.com>
2020-09-17 19:00:37 -0400

    created plot

--- activity 3.R
+++ activity 3.R
@@ -1,3 +1,5 @@
 # activity 3
 ch4 <- read.csv("/Users/emmastuart/Documents/a03/lemming_herbivory.csv")
 ch4$herbivory <- as.factor(ch4$herbivory)
+plot(ch4$CH4_Flux ~ ch4$herbivory, xlab = "Treatment", 
+     ylab = "CH4 fluxes (mgC m -2 day-1)")
