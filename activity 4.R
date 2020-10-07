6f5057dcf1c8dcf79e1e8d8fbf81e380cd529cb5
estuart3 <70380230+estuart3@users.noreply.github.com>
2020-10-07 14:04:46 -0400

    created scatterplot

--- activity 4.R
+++ activity 4.R
@@ -1,2 +1,7 @@
 #activity 4
 datB <- read.csv("/Users/emmastuart/Documents/a04/beaver_dam.csv")
+head(datB)
+plot(datB$dams.n, datB$area.ha, pch = 19, 
+     col = "royalblue4", 
+     ylab = "Surface water area (ha)",
+     xlab = "Number of beaver dams")
