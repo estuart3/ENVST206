b96c839ab5cda6d3ce46fe428709119be6566694
estuart3 <70380230+estuart3@users.noreply.github.com>
2020-11-20 08:54:41 -0500

    uploaded syracuse data

--- Project.R
+++ Project.R
@@ -3,3 +3,14 @@ utica_tavg$Year <- as.factor(utica_tavg$Year)
 utica_tavg$Tavg <- as.numeric(utica_tavg$Tavg)
 averagetemp_U <- aggregate(utica_tavg$Tavg, by = list(utica_tavg$Year), FUN = "mean", na.rm = TRUE)
 colnames(averagetemp_U) <- c("Year", "MAAT")
+plot(averagetemp_U$YEAR, averagetemp_U$MAAT,
+     pch = 19,
+     col = "royalblue4",
+     ylab = "Average annual temperature",
+     xlab = "Year")
+plot(averagetemp_U$Year, averagetemp_U$MAAT, 
+     pch = 19,
+     col = "royalblue4", 
+     ylab = "Average annual temperature", 
+     xlab = "Year")
+syracuse_tavg <- read.csv("/Users/emmastuart/Documents/Syracuse_tavg.csv")
